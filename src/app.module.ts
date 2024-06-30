d4211f86f6f9427d1fc88d6258a1ba42ed0380f1
Mauricio <88463671+Doandres@users.noreply.github.com>
2024-06-29 19:34:57 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SeedModule } from './seed/seed.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/pokemon?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.9', {
+      connectionFactory: (connection) => {
+        console.log('Conexión a MongoDB exitosa');
+        return connection;
+      },
+    }),
+
+
+    PokemonModule,
+
+
+    SeedModule,
+  ],
+})
+export class AppModule {}
